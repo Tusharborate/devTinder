3c63b6725e87e5c654ecb7cf506bbdab99a22118
Tusharborate <17202256+Tusharborate@users.noreply.github.com>
2025-12-29 18:24:48 +0530

    Adds Dynamic Routes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/test", function (req, res) {
-  res.send("GET is working");
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({ firstName: "Tushar", lastName: "Borate" });
 });
 
-app.post("/test", function (req, res) {
-  res.send("POST is working");
-});
-
-app.delete("/test", function (req, res) {
-  res.send("Data Deleted Successfully");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello from the server!!");
+app.get("/users/:userId/:userName/:passWord", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Tushar", lastName: "Borate" });
 });
 
 app.listen(7777, () => {
